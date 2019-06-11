ba143e051dacbb1474fee943b09b729feb47263e
Zaid NAZAM <znazam@c4r7s8.wethinkcode.co.za>
2019-06-11 16:13:37 +0200

    corrected

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: znazam <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 15:48:50 by znazam            #+#    #+#             */
-/*   Updated: 2019/05/28 12:00:21 by znazam           ###   ########.fr       */
+/*   Updated: 2019/06/11 16:12:59 by znazam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int		ft_atoi(const char *str)
 {
-	int	i;
-	int	n;
+	long	i;
+	int		n;
 
 	i = 0;
 	while ((*str >= '\t' && *str <= '\r') || *str == 32)
@@ -28,5 +28,9 @@ int		ft_atoi(const char *str)
 		str++;
 	while (*str && ft_isdigit(*str))
 		i = (i * 10) + (*str++ - '0');
+	if (i < 0 && n == 1)
+		return (-1);
+	if (i < 0)
+		return (0);
 	return (i * n);
 }
