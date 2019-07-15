800da923a7d8d2e6c3523df1f404f8d05d647ee4
Zaid NAZAM <znazam@c4r9s5.wethinkcode.co.za>
2019-07-15 10:32:15 +0200

    lstadd update

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: znazam <znazam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 08:29:32 by znazam            #+#    #+#             */
-/*   Updated: 2019/07/15 09:10:44 by znazam           ###   ########.fr       */
+/*   Updated: 2019/07/15 10:31:51 by znazam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 void	ft_lstadd(t_list **alst, t_list *new)
 {
-	new->next = *alst;
-	*alst = new;
-	(*alst)->previous = (*alst)->next->previous;
-	(*alst)->next->previous = (*alst);
+	if (*alst)
+	{
+		new->next = *alst;
+		*alst = new;
+		(*alst)->previous = (*alst)->next->previous;
+		(*alst)->next->previous = (*alst);
+	}
+	else
+	{
+		*alst = new;
+	}
 }
