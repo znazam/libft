c58be54311064f0bd317d53f7f08e0c9d8dea9dd
Zaid NAZAM <znazam@c4r9s5.wethinkcode.co.za>
2019-07-17 11:55:01 +0200

    makefile update

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: znazam <znazam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 08:29:16 by znazam            #+#    #+#             */
-/*   Updated: 2019/07/17 11:54:02 by znazam           ###   ########.fr       */
+/*   Updated: 2019/07/17 11:54:55 by znazam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void    ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	if(!*alst)
-		return ();
+		return ;
 	ft_lstdel(&(*alst)->next, del);
 	del((*alst)->content, (*alst)->content_size);
 	free(*alst);
